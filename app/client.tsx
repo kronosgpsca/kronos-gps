bce851edff09beaa82e2077f83226279bd1330bb
kronosgpsca <kronosgpsca@gmail.com>
2026-04-22 21:05:04 -0400

    Update client.tsx

--- app/client.tsx
+++ app/client.tsx
@@ -1,7 +1,8 @@
 import { hydrateRoot } from 'react-dom/client'
-import { StartClient } from '@tanstack/react-start'
+import { RouterProvider } from '@tanstack/react-router'
 import { createRouter } from './router'
 
 const router = createRouter()
 
-hydrateRoot(document, <StartClient router={router} />)
+// Usamos RouterProvider en lugar de StartClient
+hydrateRoot(document, <RouterProvider router={router} />)
