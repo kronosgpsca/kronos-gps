60df5a3aeefb8d5f437e407ece105ce05b11f70e
kronosgpsca <kronosgpsca@gmail.com>
2026-04-22 23:12:35 -0400

    Update client.tsx

--- app/client.tsx
+++ app/client.tsx
@@ -1,17 +1,15 @@
+import './global.css' // <--- Esta línea conecta los estilos
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { createRouter } from './router'
 
 const router = createRouter()
 
-// Forzamos el renderizado inmediato
 const rootElement = document.getElementById('root')
 
 if (rootElement) {
   const root = createRoot(rootElement)
   root.render(<RouterProvider router={router} />)
-} else {
-  console.error("No se encontró el elemento #root")
 }
 
 export default {}
