db4497fd3f487ad3cd00da3e30c5ac4d5ff03694
kronosgpsca <kronosgpsca@gmail.com>
2026-04-22 21:11:56 -0400

    Update client.tsx

--- app/client.tsx
+++ app/client.tsx
@@ -1,8 +1,14 @@
-import { hydrateRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { createRouter } from './router'
 
 const router = createRouter()
 
-// Usamos RouterProvider en lugar de StartClient
-hydrateRoot(document, <RouterProvider router={router} />)
+// Solo ejecutamos si existe 'document' (evita el error en el build)
+if (typeof document !== 'undefined') {
+  const rootElement = document.getElementById('root')
+  if (rootElement) {
+    const root = createRoot(rootElement)
+    root.render(<RouterProvider router={router} />)
+  }
+}
