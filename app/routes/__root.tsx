b93236b6f622b4379df1a24655ad7e0e049d9ee9
kronosgpsca <kronosgpsca@gmail.com>
2026-04-22 21:05:39 -0400

    Update __root.tsx

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -2,8 +2,8 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <div id="app">
       <Outlet />
-    </>
+    </div>
   ),
 })
