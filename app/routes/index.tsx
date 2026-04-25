19cc89e11c2a2e01bb6b41a924b85ad2b4dc544c
kronosgpsca <kronosgpsca@gmail.com>
2026-04-24 23:17:55 -0400

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,17 +14,17 @@ function Home() {
 
 const slides = [
   {
-    image: '/slider1.jpg', // Asegúrate de tener estas imágenes en tu carpeta public
+    image: '/Imagen1.webp',
     title: 'Control y Monitoreo en Tiempo Real',
     subtitle: 'La mejor herramienta de gestión empresarial para tu personal y flotas en Caracas.'
   },
   {
-    image: '/slider2.jpg',
+    image: '/Imagen2.webp',
     title: 'Protege tus Activos con Tecnología',
     subtitle: 'Seguridad avanzada y rastreo satelital con cobertura en toda Latinoamérica.'
   },
   {
-    image: '/slider3.jpg',
+    image: '/Imagen3.webp',
     title: 'Optimiza tus Rutas hoy mismo',
     subtitle: 'Reduce costos de combustible y mejora la puntualidad de tus entregas.'
   }
